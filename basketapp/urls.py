f4d920019bccc7c73de7edf2c1a5c3ac09efbcf9
Berezin <berezin_git@mail.ru>
2021-12-06 20:05:13 +0300

    add homework 6

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from basketapp.views import basket, basket_add, basket_remove
+from basketapp.views import basket, basket_add, basket_remove, basket_edit
 
 app_name = 'basketapp'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', basket, name='basket'),
     path('add/<int:pk>/', basket_add, name='basket_add'),
     path('remove/<int:pk>/', basket_remove, name='basket_remove'),
+    path('edit/<int:pk>/<quantity>/', basket_edit, name='basket_edit'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -5,11 +7,30 @@ from basketapp.models import Basket
 from mainapp.models import Product, ProductCategory
 
 
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    return []
+
+
+def get_hot_product():
+    products_list = Product.objects.all()
+
+    return random.sample(list(products_list), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products_list = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)
+
+    return same_products_list[:3]
+
+
 def index(request):
     products_list = Product.objects.all()[:4]
     context = {
         'title': 'Магазин',
         'products': products_list,
+        'basket': get_basket(request.user)
     }
     return render(request, 'mainapp/index.html', context)
 
@@ -28,23 +49,34 @@ def products(request, pk=None):
             'links_menu': links_menu,
             'products': products_list,
             'category': category_item,
-            'basket': Basket.objects.filter(user=request.user)
+            'basket': get_basket(request.user)
         }
 
         return render(request, 'mainapp/products_list.html', context)
-
+    hot_product = get_hot_product()
     context = {
         'links_menu': links_menu,
         'title': 'Продукты',
-        'hot_product': Product.objects.all().first(),
-        'same_products': Product.objects.all()[3:5],
-        'basket': Basket.objects.filter(user=request.user)
+        'hot_product': hot_product,
+        'same_products': get_same_products(hot_product),
+        'basket': get_basket(request.user)
     }
     return render(request, 'mainapp/products.html', context)
 
 
 def contact(request):
     context = {
-        'title': 'Контакты'
+        'title': 'Контакты',
+        'basket': get_basket(request.user)
     }
     return render(request, 'mainapp/contact.html', context)
+
+
+def product(request, pk):
+    links_menu = ProductCategory.objects.all()
+    context = {
+        'product': get_object_or_404(Product, pk=pk),
+        'links_menu': links_menu,
+        'basket': get_basket(request.user),
+    }
+    return render(request, 'mainapp/product.html', context)
